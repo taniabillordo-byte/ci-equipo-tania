027273334ff7c7987fe4bdd65818e7689e3b3fd2
Tania <tania.billordo@cablasociados.com>
2026-04-29 13:12:52 -0300

    Agregando el archivo de workflow de GitHub Actions

---
+++ ci-equipo-tania/src/MiApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
